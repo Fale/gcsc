121fc001558b00b2aabb43b9e5606c1f5040095a
Fabio Alessandro Locati <me@fale.io>
2019-09-16 21:51:40 +0200

    remove pubsub reference since there is no code yet

--- init.go
+++ init.go
@@ -12,13 +12,6 @@ func initCmd() *cobra.Command {
 	rootCmd.PersistentFlags().Bool("automatic", true, "Include automatic backups")
 	rootCmd.PersistentFlags().Bool("manual", false, "Include manual backups")
 
-	var pubsubCmd = &cobra.Command{
-		Use:   "pubsub",
-		Short: "listen to a pub/sub stream",
-		//    RunE:  serve,
-	}
-	rootCmd.AddCommand(pubsubCmd)
-
 	var cleanCmd = &cobra.Command{
 		Use:   "clean",
 		Short: "execute a cleaning",
